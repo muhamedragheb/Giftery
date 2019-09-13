98990b61cd8b18bc18912e6b94799adb92fa8ada
ahmedsaeid98 <asaeid2747@gmail.com>
2019-09-14 01:36:41 +0200

    fix slider

--- js/index4.js
+++ js/index4.js
@@ -14,20 +14,60 @@ $(document).ready(function(){
 
   */
 
+let allBiggerImage=document.getElementsByClassName("bigger");
+let allBiggerImage_array=[];
+for(let i=0;i<allBiggerImage.length;i++)
+{
+  allBiggerImage_array[i]=allBiggerImage[i]; 
+}
+let allSmallImage=document.getElementsByClassName("smallImg");
+let allSmallImage_array=[];
+for(let i=0;i<allSmallImage.length;i++)
+{
+  allSmallImage_array[i]=allSmallImage[i]; 
+}
+let currentImg=0;
 
-// with smoothing
+let prev_btn=document.getElementById("prev-img");
+let next_btn=document.getElementById("next-img");
 
- $(".smallImg").click(function(){
-    
-  let imgs =$(this).attr("src");
-  $('#bigImg').fadeTo(500,0.5,function(){
-    $("#bigImg").attr("src",imgs)  
-    $('#bigImg').fadeTo(500,1);
-  
-  });
-  })
-  
+prev_btn.addEventListener("click",function(e){
 
+  let temp=currentImg-1;
+  if(temp < 0)
+  {
+    temp=allBiggerImage_array.length-1;
+  }
+  $(allBiggerImage_array[currentImg]).fadeOut(1000);
+  $(allBiggerImage_array[temp]).fadeIn(1000);
+  currentImg=temp;  
+})
+
+next_btn.addEventListener("click",function(e){
+
+  let temp=currentImg+1;
+  if(temp > allBiggerImage_array.length-1)
+  {
+    temp=0;
+  }
+  $(allBiggerImage_array[currentImg]).fadeOut(1000);
+  $(allBiggerImage_array[temp]).fadeIn(1000);
+  currentImg=temp;  
+})
+
+
+$(".smallImg").click(function(e){
+
+  let index=allSmallImage_array.indexOf(e.target);
+
+  if(currentImg != index )
+  {
+     $(allBiggerImage_array[currentImg]).fadeOut(1000);
+     $(allBiggerImage_array[index]).fadeIn(1000);
+     currentImg=index;
+   }
+
+})
   
   
 /* for Reviews Section */
